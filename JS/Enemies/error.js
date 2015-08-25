bbb5375f3b63255c92dd1c54d699093dca4e48f8
Saint Jakuzi <saint3543jakuzi@gmail.com>
2015-08-25 21:22:39 +0300

    Fixed bug with error + speed
    Now error spawn at one point and not go throw walls. Speed increased to
    2 (now its uniform)

--- JS/Enemies/error.js
+++ JS/Enemies/error.js
@@ -2,54 +2,40 @@ Crafty.c("Error", {
     init: function () {
         this.addComponent("2D, DOM, error, Collision");
         this.bind("spawn", function () {
-            /*this.attr({x: 3 * Settings.poligon, y: 7 * Settings.poligon})*/
             var random = (Math.random() * 100);
-            if (random >= 0 && random <= 50) {
-                this.attr({x: 3 * Settings.poligon, y: 3 * Settings.poligon})
-            }
-            if (random > 50 && random <= 100) {
-                this.attr({x: 16 * Settings.poligon, y: 7 * Settings.poligon})
-            }
+            this.attr({x: 16 * Settings.poligon, y: 7 * Settings.poligon})
         })
         this.trigger("spawn");
         var directions = [
-            {name: 'ErrorUp', x: 0, y: -1},
-            {name: 'ErrorRight', x: 1, y: 0},
-            {name: 'ErrorLeft', x: -1, y: 0},
-            {name: 'ErrorDown', x: 0, y: 1}
+            {name: 'ErrorUp', x: 0, y: -2},
+            {name: 'ErrorRight', x: 2, y: 0},
+            {name: 'ErrorLeft', x: -2, y: 0},
+            {name: 'ErrorDown', x: 0, y: 2}
         ];
         this.bind("EnterFrame", function () {
             var posx = this.x;
-            if (this.y > 59 && this.x == 60)
-            {
+            if (this.y > 59 && this.x == 60) {
                 var direction = directions[0];
                 this.x += direction.x;
                 this.y += direction.y;
             }
-            if (this.y < 61 && posx <= 319)
-            {
+            if (this.y < 61 && posx <= 319) {
                 var direction = directions[1];
                 this.y += direction.y;
                 this.x += direction.x;
             }
 
-            if (this.x == 320 && this.y < 140)
-            {
+            if (this.x == 320 && this.y < 140) {
                 var direction = directions[3];
-                this.y += 2;
+                this.y += direction.y;
                 this.x += direction.x;
             }
-            if (this.x >= 60 && this.y == 141 || this.y == 140 || this.y == 142)
-            {
+            if (this.x >= 60 && this.y == 141 || this.y == 140 || this.y == 142) {
                 var direction = directions[2];
                 this.y += 0;
                 this.x += direction.x;
             }
-
-
-
         })
-
         this.trigger("EnterFrame");
     },
 });
